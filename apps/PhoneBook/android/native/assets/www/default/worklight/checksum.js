6ecc2d17ec1bae8341ee7e02aba6594dfac9786c
sreeks23 <Sreeks@Sreekanths-MacBook-Pro.local>
2015-02-19 21:56:08 -0800

    From Mac
    Feb 19th

--- apps/PhoneBook/android/native/assets/www/default/worklight/checksum.js
+++ apps/PhoneBook/android/native/assets/www/default/worklight/checksum.js
@@ -1,2 +1,2 @@
-var WL_CHECKSUM = {"checksum":3832275797,"date":1424408783925,"machine":"IBM938-PBT0VH9"};
-/* Date: Thu Feb 19 21:06:23 PST 2015 */
+var WL_CHECKSUM = {"checksum":1450250638,"date":1424409702920,"machine":"Sreekanths-MacBook-Pro.local"};
+/* Date: Thu Feb 19 21:21:42 PST 2015 */
